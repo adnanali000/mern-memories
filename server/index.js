88b4c630c78959bcbb51e11fac43c53a811d4ef8
adnanali000 <adnan.museel123@gmail.com>
2022-04-19 13:42:15 +0500

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+const app = express();
+
+dotenv.config();
+
+//setting body parser to send our request
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+//db connection
+const CONNECTION_URL = process.env.APP_CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT , () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error)=>console.log(error.message))
+
