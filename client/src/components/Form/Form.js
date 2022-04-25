b7a4dcc78935537032d869b010597b9588c241d9
adnanali000 <adnan.museel123@gmail.com>
2022-04-25 11:22:12 +0500

    update post work done

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,21 +1,32 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import useStyles from './styles'
 import { TextField,Paper,Button,Typography } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import {useDispatch} from 'react-redux'
-import { createPost } from '../../actions/posts';
+import {useDispatch , useSelector} from 'react-redux'
+import { createPost,updatePost } from '../../actions/posts';
 
 
-const Form = ()=>{
+const Form = ({currentId,setCurrentId})=>{
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    const post = useSelector((state)=> currentId ? state.posts.find((p)=> p._id === currentId) : null);
+
     const [postData,setPostData] = useState({ creator:'',title:'',message:'',tags:'',selectedFile:''})
+
+    useEffect(()=>{
+        if(post) setPostData(post);
+    },[post])
 
     const handleSubmit = (e)=>{
         e.preventDefault();
 
-        dispatch(createPost(postData))
+        if(currentId){
+            dispatch(updatePost(currentId,postData))
+        }else{
+            dispatch(createPost(postData))
+        }
+
     }
     const clear = ()=>{
 
